d4b411e2d2250e2a580b739808ecea0ad9ff6db6
bibiefrat <bibi.efrat@gmail.com>
2023-04-17 22:08:40 +0300

    first draft

--- flask_web_app/app.py
+++ flask_web_app/app.py
@@ -1,9 +1,12 @@
 from bson.objectid import ObjectId
 from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
+import os
 
+mongo = os.getenv('MONGO')
 app = Flask(__name__)
-client = MongoClient('mongo', 27017)
+client = MongoClient(mongo, 27017)
+print(f"mongo client: {client}")
 db = client.flask_db
 todos = db.todos
 
@@ -16,7 +19,6 @@ def index():
         degree = request.form['degree']
         todos.insert_one({'content': content, 'degree': degree})
         return redirect(url_for('index'))
-
     all_todos = todos.find()
     return render_template('index.html', todos=all_todos)
 
