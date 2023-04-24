6c1f113fcf404577acbfce0b92fc23ad7832f094
bibiefrat <bibi.efrat@gmail.com>
2023-04-24 19:44:24 +0300

    first draft

--- flask_web_app/app.py
+++ flask_web_app/app.py
@@ -1,16 +1,31 @@
 from bson.objectid import ObjectId
 from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
+from urllib.parse import quote_plus
 import os
 
-mongo = os.getenv('MONGO')
-app = Flask(__name__)
-client = MongoClient(mongo, 27017)
-print(f"mongo client: {client}")
-db = client.flask_db
+
+#mongo = os.getenv('MONGO')
+username= os.environ.get('MONGO_INITDB_ROOT_USERNAME', 'bibi')
+password = os.environ.get('MONGO_INITDB_ROOT_PASSWORD', '029365947')
+mongo_host = os.environ.get('MONGO', 'localhost')
+mongodb_port = int(os.environ.get('MONGO_PORT', '27017'))
+#myclient = MongoClient(host=[f"{mongo_host}:{mongodb_port}"], username=username, password=password)
+#myclient = MongoClient(username=username, password=password)
+myclient = MongoClient(host=[f"{mongo_host}:{mongodb_port}"])
+#host = f"{mongo}:27017"
+#uri = "mongodb://%s:%s@%s" % (quote_plus(username), quote_plus(password), host)
+# Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+#client = MongoClient(mongo, 27017,username, password)
+#client = MongoClient(uri)
+#client = MongoClient('localhost', 27017, username='bibi', password='029365947')
+print(f"my mongo client: {myclient}")
+print(f"u p: {username}: {password}")
+db = myclient.flask_db
 todos = db.todos
 
 
+app = Flask(__name__)
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
